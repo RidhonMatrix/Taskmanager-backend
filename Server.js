3f4847906fc2a9275e47283f3360f6add4407042
ridhonmatrix <rezaulkarim.ridhon@gmail.com>
2024-01-25 15:47:41 +0600

    updated

--- Server.js
+++ Server.js
@@ -7,7 +7,7 @@ const routes = require("./routes/ToDoRoutes");
 const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 4001;
 
 // Middleware
 app.use(express.json());
